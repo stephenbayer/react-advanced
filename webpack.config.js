db7d33b629a089bb7315aebfc874bed4700ebdfd
Stephen Bayer <sabayer@gmail.com>
2018-06-13 13:16:48 -0700

    adding api, initial tests, and server side rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './lib/components/Index.js',
+  entry: ['babel-polyfill', './lib/components/Index.js'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
